71f331a4c1082ab5a996250da2e8c8bbbb17a3f9
manimaran <154576082+Maniharini@users.noreply.github.com>
2025-07-19 20:01:42 +0530

    Create center.py

---
+++ center.py
@@ -0,0 +1,7 @@
+a=int(input("enter a  starting number:"))
+b=int(input("enter a ending  number:"))
+c=int(input("enter   number:" ))
+if(c>=a and c<=b):
+    print("center number")
+else:
+    print("not center number")
